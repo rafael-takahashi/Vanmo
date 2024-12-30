367fdc2599800b7fe3e6e53958691a4f848a2466
Leandro Eugênio Farias Berton <95644934+Leandroefb@users.noreply.github.com>
2024-12-30 11:38:06 -0300

    Implementação de algumas verificações para o veículo (disponibilidade, empresa)

--- backend/cruds/crud_veiculo.py
+++ backend/cruds/crud_veiculo.py
@@ -5,6 +5,7 @@ from decimal import *
 from classes.classe_usuario import *
 from database import *
 import sqlite3
+import datetime
 
 def criar_veiculo():
     pass
@@ -22,3 +23,19 @@ def listar_veiculos():
 
 def atualizar_veiculo():
     pass
+
+def verificar_veiculo_empresa(db: sqlite3.Connection, id_veiculo: int, id_empresa: int) -> bool:
+    cursor = db.cursor()
+    dados = (id_veiculo, id_empresa)
+
+    resultado = cursor.execute(QueriesDB.query_verificar_veiculo_empresa, dados).fetchone()
+    
+    return resultado is not None
+
+def verificar_disponibilidade_veiculo(db: sqlite3.Connection, id_veiculo: int, data_inicio: datetime.date, data_fim: datetime.date) -> bool:
+    cursor = db.cursor()
+    dados = (id_veiculo, data_inicio, data_fim)
+
+    resultado = cursor.execute(QueriesDB.query_verificar_disponibilidade_veiculo, dados).fetchone()
+
+    return resultado is not None
