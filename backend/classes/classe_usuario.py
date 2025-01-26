0333d928618f14ed36b0f8da1dc0b5c0bc4b00e7
Leandro Eugênio Farias Berton <95644934+Leandroefb@users.noreply.github.com>
2025-01-26 09:55:23 -0300

    Inclusão do telefone no cadastro da empresa + passagem da senha_hashed como parametro na criação de empresa/cliente

--- backend/classes/classe_usuario.py
+++ backend/classes/classe_usuario.py
@@ -7,27 +7,27 @@ from classes.classe_endereco import Endereco
 from classes.classe_local import Local
 
 class Usuario():
-    def __init__(self, email: str, senha_hashed: str, tipo_conta: str, foto: str | None = None, id: int | None = None):
+    def __init__(self, email: str, senha_hashed: str, tipo_conta: str, foto: str | None = None, telefone: str | None = None, id: int | None = None):
         self.id: int | None = id
         self.email: str = email
         self.senha_hashed: str = senha_hashed
         self.tipo_conta: str = tipo_conta
         self.foto: str | None = foto
+        self.telefone: str = telefone
 
 class Cliente(Usuario):
     def __init__(self, id, email, senha_hashed, tipo_conta, foto, nome_completo: str, cpf: str, data_nascimento: str, telefone: str):
-        super().__init__(id, email, senha_hashed, tipo_conta, foto)
+        super().__init__(email, senha_hashed, tipo_conta, foto, telefone, id)
         self.nome_completo: str = nome_completo
         self.cpf: str = cpf
         self.data_nascimento: str = data_nascimento
-        self.telefone: str = telefone
         
     def __repr__(self):
         return f"[Cliente] Id: {self.id} email: {self.email} nome: {self.nome_completo} cpf: {self.cpf}"
     
 class Empresa(Usuario):
-    def __init__(self, id, email, senha_hashed, tipo_conta, foto, nome_fantasia: str, cnpj: str, endereco: Endereco, local: Local):
-        super().__init__(id, email, senha_hashed, tipo_conta, foto)
+    def __init__(self, id, email, senha_hashed, tipo_conta, foto, nome_fantasia: str, cnpj: str, endereco: Endereco, local: Local, telefone: str):
+        super().__init__(email, senha_hashed, tipo_conta, foto, telefone, id)
         self.nome_fantasia: str = nome_fantasia
         self.cnpj: str = cnpj
         self.endereco: Endereco = endereco
--- backend/main.py
+++ backend/main.py
@@ -121,6 +121,7 @@ class CadastroEmpresa(BaseModel):
     email: str
     senha: str
     nome_fantasia: str
+    telefone: str
     cnpj: str
     uf: str
     cidade: str
@@ -141,11 +142,11 @@ def registrar_novo_usuario(dados: CadastroUsuario) -> int:
     
     usuario.senha_hashed = auth.gerar_hash_senha(usuario.senha_hashed)
     id_usuario = crud_usuario.criar_usuario(db, usuario)
-    return id_usuario
+    return id_usuario, usuario.senha_hashed
 
 @app.post("/usuario/cadastro/empresa")
 async def registrar_empresa(dados: CadastroEmpresa):
-    id_usuario = registrar_novo_usuario(dados.email, dados.senha, "empresa", "")
+    id_usuario, senha = registrar_novo_usuario(dados.email, dados.senha, "empresa", "")
 
     db = database.conectar_bd()
 
@@ -160,13 +161,15 @@ async def registrar_empresa(dados: CadastroEmpresa):
 
     # TODO: Validar CNPJ
 
+    # TODO: Revisar como é feita a passagem da senha ao criar a empresa
+
     latitude = 0
     longitude = 0
 
     local: classe_local.Local = classe_local.Local(latitude, longitude, dados.nome_fantasia)
 
-    empresa: classe_usuario.Empresa = classe_usuario.Empresa(id=id_usuario, email="", senha_hashed="", tipo_conta="empresa", foto="",
-                                                             nome_fantasia=dados.nome_fantasia, cnpj=dados.cnpj, endereco=endereco, local=local)
+    empresa: classe_usuario.Empresa = classe_usuario.Empresa(id=id_usuario, email=dados.email, senha_hashed=senha, tipo_conta="empresa", foto="",
+                                                             nome_fantasia=dados.nome_fantasia, cnpj=dados.cnpj, endereco=endereco, local=local, telefone=dados.telefone)
     
     empresa.id = id_usuario
 
@@ -176,13 +179,15 @@ async def registrar_empresa(dados: CadastroEmpresa):
 
 @app.post("/usuario/cadastro/cliente")
 async def registrar_cliente(dados: CadastroCliente):
-    id_usuario = registrar_novo_usuario(dados.email, dados.senha, "cliente", "")
+    id_usuario, senha = registrar_novo_usuario(dados.email, dados.senha, "cliente", "")
 
     db = database.conectar_bd()
 
     # TODO: Validar CPF
 
-    cliente: classe_usuario.Cliente = classe_usuario.Cliente(id_usuario, "", "", "cliente", "", dados.nome_completo, dados.cpf, dados.data_nascimento, dados.telefone)
+    # TODO: Revisar como é feita a passagem da senha ao criar o cliente
+
+    cliente: classe_usuario.Cliente = classe_usuario.Cliente(id_usuario, dados.email, senha, "cliente", "", dados.nome_completo, dados.cpf, dados.data_nascimento, dados.telefone)
 
     cliente.id = id_usuario
 
