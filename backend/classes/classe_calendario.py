0989b35613d62da0d0243e512f63fb23f417bac9
Leandro Eugênio Farias Berton <95644934+Leandroefb@users.noreply.github.com>
2025-01-28 21:25:47 -0300

    Conversão de str para date na classe calendário

--- backend/classes/classe_calendario.py
+++ backend/classes/classe_calendario.py
@@ -1,12 +1,14 @@
 from typing import List
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 class Calendario():
     def __init__(self, datas_indisponiveis: List[date]):
         # TODO: Usar set ao invés de list
         self.datas_indisponiveis = datas_indisponiveis
     
-    def adicionar_datas_indisponiveis(self, data_inicio: date, data_fim: date):
+    def adicionar_datas_indisponiveis(self, data_inicio: str, data_fim: str):
+        data_inicio = datetime.strptime(data_inicio, "%Y-%m-%d")
+        data_fim = datetime.strptime(data_fim, "%Y-%m-%d")
         incremento_dia = timedelta(days=1)
         while (data_inicio <= data_fim):
             self.datas_indisponiveis.append(data_inicio)
